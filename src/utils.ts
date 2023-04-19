579b96f651fceee60c5f4706e0216a4517b2a0e8
Delton Chua <57023602+deltonchua@users.noreply.github.com>
2023-04-19 07:16:39 +0300

    Fix time format

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,7 @@
 export const formatTime = (dt: Date) => {
+  const hours = dt.getHours();
+  const minutes = dt.getMinutes().toString();
   return `${dt.getDate()}-${dt.getMonth() + 1}-${dt.getFullYear()} ${
-    dt.getHours() || 12
-  }:${dt.getMinutes()}${dt.getHours() > 11 ? 'pm' : 'am'}`;
+    hours || 12
+  }:${minutes.length > 1 ? minutes : '0' + minutes}${hours > 11 ? 'pm' : 'am'}`;
 };
